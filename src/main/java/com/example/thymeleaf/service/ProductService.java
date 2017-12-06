e7de2a785d21e2760ac6cf12ff0a709ef2b40ad9
liuwenqi528 <410587745@qq.com>
2017-12-06 09:56:24 +0800

    添加了一个注释

--- src/main/java/com/example/thymeleaf/service/ProductService.java
+++ src/main/java/com/example/thymeleaf/service/ProductService.java
@@ -4,6 +4,9 @@ import com.example.thymeleaf.entity.Product;
 
 import java.util.List;
 
+/**
+ * 产品服务层
+ */
 public interface ProductService {
     public List<Product> findAll();
     public Product findById(Integer  id) ;
